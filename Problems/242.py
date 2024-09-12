92effee7cc6c9ee8c78e50cbd069793b92e09f35
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-09-12 20:55:54 +0400

    added problems

---
+++ Problems/242.py
@@ -0,0 +1,62 @@
+# 242. Valid Anagram
+# Solved
+# Easy
+# Topics
+# Companies
+# Given two strings s and t, return true if t is an 
+# anagram
+#  of s, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+ 
+
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        
+        char_count = {}
+
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+        
+        for char in t:
+            if char in char_count:
+                char_count[char] -= 1
+                if char_count[char]<0:
+                    return False
+            else:
+                return False
+        
+        return True
+        
+
+        
+
+
+
---
+++ Problems/49.py
@@ -0,0 +1,49 @@
+# 49. Group Anagrams
+# Solved
+# Medium
+# Topics
+# Companies
+# Given an array of strings strs, group the 
+# anagrams
+#  together. You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Explanation:
+
+# There is no string in strs that can be rearranged to form "bat".
+# The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+# The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+# Example 2:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+
+# Example 3:
+
+# Input: strs = ["a"]
+
+# Output: [["a"]]
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        anagrams = {}
+      
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+
+            if sorted_word not in anagrams:
+                anagrams[sorted_word] = [word]
+            else:
+                 anagrams[sorted_word].append(word)
+        
+        return list(anagrams.values())
+
